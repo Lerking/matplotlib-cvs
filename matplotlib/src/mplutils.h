5b27f8c46595d044fab2749d31889f34b7af7514
jdh2358 <jdh2358>
2004-06-24 20:11:14 +0000

    added mpl src

---
+++ matplotlib/src/mplutils.h
@@ -0,0 +1,17 @@
+/* mplutils.h	-- 
+ *
+ * $Header: /home/pauli/tmp/mpl/matplotlib/matplotlib/src/mplutils.h,v 1.1 2004-06-24 20:11:17 jdh2358 Exp $
+ * $Log: mplutils.h,v $
+ * Revision 1.1  2004-06-24 20:11:17  jdh2358
+ * added mpl src
+ *
+ */
+
+#ifndef _MPLUTILS_H
+#define _MPLUTILS_H
+
+#include <string>
+#include <iostream>
+
+void _VERBOSE(const std::string&);
+#endif
