4270fb11929726a69223399b2d2193dae3e2f22e
stevech <stevech>
2004-10-01 08:55:02 +0000

    SC 01/10/2004

--- matplotlib/lib/matplotlib/backends/backend_gtk.py
+++ matplotlib/lib/matplotlib/backends/backend_gtk.py
@@ -16,6 +16,8 @@ if gtk.pygtk_version < version_required:
     raise SystemExit ("PyGTK %d.%d.%d is installed\n"
                       "PyGTK %d.%d.%d or later is required"
                       % (gtk.pygtk_version + version_required))
+backend_version = gtk.pygtk_version
+
 from gtk import gdk
 import pango
 
@@ -45,6 +47,7 @@ else: useMathText = True
 PIXELS_PER_INCH = 96
 
 backend_version = 'unknown'
+
 cursord = {
     cursors.MOVE          : gtk.gdk.Cursor(gtk.gdk.FLEUR),
     cursors.HAND          : gtk.gdk.Cursor(gtk.gdk.HAND2),
