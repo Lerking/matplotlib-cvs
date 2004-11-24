0c9d463cf52e8888fb5f4992679688e72b4fda0f
jdh2358 <jdh2358>
2004-11-24 15:25:59 +0000

    added Printf

--- matplotlib/src/mplutils.h
+++ matplotlib/src/mplutils.h
@@ -1,8 +1,11 @@
 /* mplutils.h	-- 
  *
- * $Header: /home/pauli/tmp/mpl/matplotlib/matplotlib/src/mplutils.h,v 1.1 2004-06-24 20:11:17 jdh2358 Exp $
+ * $Header: /home/pauli/tmp/mpl/matplotlib/matplotlib/src/mplutils.h,v 1.2 2004-11-24 15:26:12 jdh2358 Exp $
  * $Log: mplutils.h,v $
- * Revision 1.1  2004-06-24 20:11:17  jdh2358
+ * Revision 1.2  2004-11-24 15:26:12  jdh2358
+ * added Printf
+ *
+ * Revision 1.1  2004/06/24 20:11:17  jdh2358
  * added mpl src
  *
  */
@@ -12,6 +15,20 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
 
 void _VERBOSE(const std::string&);
+
+
+class Printf
+{
+private :
+  char *buffer;
+public :
+  Printf(const char *, ...);
+  ~Printf();
+  std::string str() {return buffer;}
+  friend std::ostream &operator <<(std::ostream &, const Printf &);
+};
+
 #endif
