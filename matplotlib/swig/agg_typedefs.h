1ccccb20ab62daed944f5bc2ff9c6e5ce52899e1
jdh2358 <jdh2358>
2005-06-23 22:24:29 +0000

    some work on agg swig wrapper

--- matplotlib/swig/agg_typedefs.h
+++ matplotlib/swig/agg_typedefs.h
@@ -1,14 +1,21 @@
 /* typedefs.h	-- John Hunter
  *
- * $Header: /home/pauli/tmp/mpl/matplotlib/matplotlib/swig/agg_typedefs.h,v 1.1 2005-05-12 19:16:35 jdh2358 Exp $
+ * $Header: /home/pauli/tmp/mpl/matplotlib/matplotlib/swig/agg_typedefs.h,v 1.2 2005-06-23 22:25:10 jdh2358 Exp $
  * $Log: agg_typedefs.h,v $
- * Revision 1.1  2005-05-12 19:16:35  jdh2358
+ * Revision 1.2  2005-06-23 22:25:10  jdh2358
+ * some work on agg swig wrapper
+ *
+ * Revision 1.1  2005/05/12 19:16:35  jdh2358
  * cleaned up agg wrapper
  *
  */
 
 #ifndef _TYPEDEFS_H
 #define _TYPEDEFS_H
+//#include "agg_span_interpolator_linear.h"
+//#include "agg_span_image_filter_rgba.h"
+
+
 typedef agg::path_storage path_t;
 typedef agg::conv_stroke<path_t> stroke_t;
 typedef agg::conv_transform<path_t, agg::trans_affine> transpath_t;
@@ -21,5 +28,9 @@ typedef agg::conv_curve<transpath_t> curvetrans_t;
 typedef agg::conv_stroke<curvetrans_t> strokecurvetrans_t;
 typedef agg::pixel_formats_rgba<agg::blender_rgba32, agg::pixel32_type> pixfmt_rgba_t;
 typedef agg::renderer_base<pixfmt_rgba_t> renderer_base_rgba_t;
+
+//typedef agg::span_interpolator_linear<agg::trans_affine>   interpolator_linear_t;
+
+//typedef agg::span_image_filter_rgba_nn<agg::rgba8, agg::order_rgba, interpolator_linear_t> span_imfilt_rgba_nn_interplinear_t;
 
 #endif
